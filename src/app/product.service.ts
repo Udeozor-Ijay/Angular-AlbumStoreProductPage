7b1e12882d17587689b4c66ddcbd86e00b587217
Udeozor-Ijay <92553461+Udeozor-Ijay@users.noreply.github.com>
2021-11-10 09:03:35 +0100

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,6 +4,11 @@ import {Http, Response} from '@angular/http';
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  private _albumUrl = '../assets/album.json';
+  
+  constructor(private _http: Http) { }
 
+  getAlbum(id:number){
+    return this._http.get(this._albumUrl)
+  }
 }
