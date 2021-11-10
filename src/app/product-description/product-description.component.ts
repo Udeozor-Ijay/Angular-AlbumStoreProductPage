fcc9a1e5be80f00a09bf7779c299743e31f5c304
Udeozor-Ijay <92553461+Udeozor-Ijay@users.noreply.github.com>
2021-11-10 09:25:07 +0100

    Update product-description.component.ts

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,6 +8,7 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
+  albumInfo;
 
   constructor(private _productService: ProductService) { }
 
