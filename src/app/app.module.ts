f9149a4e3d8740e68c42318b50b4ba3f49685ebb
Udeozor-Ijay <92553461+Udeozor-Ijay@users.noreply.github.com>
2021-11-13 18:57:44 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,10 @@ import { ProductListComponent } from './product-list/product-list.component';
 
 import {RouterModule , Routes} from '@angular/router';
 
-const appRoutes : Routes = [];
+const appRoutes : Routes = [
+  {path: 'products', component : ProductListComponent },
+  { path: product/:id, component : ProductPageComponent}
+];
 
 @NgModule({
   declarations: [
@@ -25,7 +28,8 @@ const appRoutes : Routes = [];
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
